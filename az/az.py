2923522f3d1a56007cc72a35b1d0229b9a2ba0ad
Sandeep More <moresandeep@gmail.com>
2020-08-04 18:50:49 -0400

    Almost working, needs some code cleanup

--- az/az.py
+++ az/az.py
@@ -8,6 +8,7 @@ from azure.mgmt.msi import ManagedServiceIdentityClient
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.resource.policy import PolicyClient
 from azure.mgmt.authorization import AuthorizationManagementClient
+from azure.mgmt.authorization.models import RoleAssignmentProperties
 from azure.mgmt.authorization.models import (Permission, RoleDefinitionProperties)
 from azure.mgmt.storage import StorageManagementClient
 from azure.storage.filedatalake import DataLakeServiceClient
@@ -98,7 +99,6 @@ class AzureFactory:
         self.client_secret_credential = ClientSecretCredential(os.environ['AZURE_TENANT_ID'], os.environ['AZURE_CLIENT_ID'], os.environ['AZURE_CLIENT_SECRET'])
 
         self.resource_client = ResourceManagementClient(self.credentials, self.subscription_id)
-        self.authorization_client = AuthorizationManagementClient(self.credentials, self.subscription_id)
         self.msi_client = ManagedServiceIdentityClient(self.credentials, self.subscription_id)
         self.policy_client = PolicyClient(self.credentials, self.subscription_id)
         self.authorization_client = AuthorizationManagementClient(self.credentials, self.subscription_id)
@@ -111,12 +111,14 @@ class AzureFactory:
         return "Microsoft"
 
     def build(self, ddf):
-        datalakename = ddf['datalake']
+        self.datalakename = ddf['datalake']
+        self.resource_group = os.environ.get('AZURE_RESOURCE_GROUP',
+                                             self.datalakename + 'RG')  # your Azure resource grou
         storage = dict()
         for name, path in ddf['storage'].items():
             # build dictionary for storage locations
             storage[name] = path['path']
-        print('Building ' + self.vendor() + ' Cloud artifacts for datalake named: ' + datalakename + '...')
+        print('Building ' + self.vendor() + ' Cloud artifacts for datalake named: ' + self.datalakename + '...')
         # print(storage)
 
         for name, role in ddf['datalake_roles'].items():
@@ -145,19 +147,14 @@ class AzureFactory:
                             t = Template(reader.read())
                             t = t.safe_substitute(d)
 
-                        filename = datalake_dir + '/' + datalakename + '/Azure/' + role['iam_role'] + '-policy.json'
-                        suffix = ''
+                        filename = datalake_dir + '/' + self.datalakename + '/Azure/' + perm_name + '-policy.json'
+                        # If policy exists do not create it again
                         if os.path.exists(filename):
-                            suffix = str(i)
+                            continue
                         # open output file
-                        with open(
-                                datalake_dir + '/' + datalakename + '/Azure/' + role[
-                                    'iam_role'] + '-policy' + suffix + '.json',
-                                'w') as writer:
+                        with open(filename, 'w') as writer:
                             writer.write(t)
-                        print('The datalake role: ' + name + ' is assigned the iam role: ' +
-                              role['iam_role'] + ' which has been granted: ' + perm_name +
-                              ' for path: ' + d['storage_location'])
+                        print(f"Done creating policy file {filename} for permission {perm_name}")
                     else:
                         print('Unknown permissions element: ' + elements[1] + ' check permissions in ddf file')
                 elif elements[0] == 'sts':
@@ -170,29 +167,25 @@ class AzureFactory:
                         with open(filepath, 'r') as reader:
                             t = Template(reader.read())
                             t = t.safe_substitute(d)
-                        filename = datalake_dir + '/' + datalakename + '/Azure/' + role['iam_role'] + '-policy.json'
-                        suffix = ''
+                        filename = datalake_dir + '/' + self.datalakename + '/Azure/' + 'assume-roles' + '-policy.json'
+                        # if policy already exists do not create more
                         if os.path.exists(filename):
-                            suffix = '-' + str(i)
+                            continue
                         # open output file
-                        with open(
-                                datalake_dir + '/' + datalakename + '/Azure/' + role[
-                                    'iam_role'] + '-policy' + suffix + '.json',
-                                'w') as writer:
+                        with open(filename, 'w') as writer:
                             writer.write(t)
-                        print('The datalake role: ' + name + ' is assigned the iam role: ' +
-                              role['iam_role'] + ' which has been granted: assumeRoles')
-                # elif elements[0] == 'db':
-
+                        print(f"Done creating policy file {filename} for permission assume-role")
                 i = i + 1
 
     def push(self, ddf):
+        # FIXME uncomment
         self.create_identities_attach_policies(ddf)
         self.create_storage_attach_MSI(ddf)
 
     def create_storage_attach_MSI(self, ddf):
         #self.create_storage_account_if_not_exist()
         self.create_containers(ddf)
+        self.assign_msi_to_container(ddf)
 
     # Create containers
     def create_containers(self, ddf):
@@ -262,24 +255,33 @@ class AzureFactory:
     and create custom roles, if they do not exist and then attach them 
     to the MSIs.
     '''
-
     def create_identities_attach_policies(self, ddf):
         self.datalakename = ddf['datalake']
+        role_permissions_map = self.get_roles_permissions_map(ddf)
         for name, role in ddf['datalake_roles'].items():
             role_name = role['iam_role']
+            # Create MSIs
             user_assigned_identity = self.create_MSI(role_name)
-            path = 'datalakes/' + ddf['datalake'] + '/Azure/'
-            filename_base = path + role_name + '-policy.json'
+            permissions = role_permissions_map.get(role_name)
+            # Create policies for all permissions
+            for resource, perm, storage_path in permissions:
+                rules = self.get_rules_for_permission(ddf, perm)
+                self.create_policy_definition(rules['Name'], rules)
+                # For storage the policies are assigned at storage account/container level
+                # so we skip applying it here.
+                if resource != 'storage':
+                    self.assign_policy_to_msi(user_assigned_identity.principal_id, rules['Name'])
 
-            if os.path.exists(filename_base):
-                with open(filename_base, "r") as policy:
-                    rules = json.load(policy)
-            else:
-                raise ValueError(f"Could not load policy file {filename_base}")
-
-            self.create_policy_definition(rules['Name'], rules)
-
-            self.assign_policy_to_msi(user_assigned_identity.principal_id, rules['Name'])
+    # Function to get a list of all permissions defined in DDF
+    # This function returns an unordered set of permission
+    def get_permissions_list(self, ddf):
+        permissions = set()
+        for name, role in ddf['datalake_roles'].items():
+            perm = role['permissions']
+            for p in perm:
+                elements = p.split(':')
+                permissions.add(elements[1])
+        return permissions
 
     def create_MSI(self, identity):
         self.resource_group = os.environ.get('AZURE_RESOURCE_GROUP',
@@ -338,10 +340,7 @@ class AzureFactory:
 
     def create_policy_definition(self, role_name, rules):
         # Get "Contributor" built-in role as a RoleDefinition object
-        roles = list(self.authorization_client.role_definitions.list(
-            self.resource_group_object.id,
-            filter="roleName eq '{}'".format(role_name)
-        ))
+        roles = self.get_azure_policy(role_name)
 
         if len(roles):
             print(f"Role {role_name} already exists")
@@ -371,11 +370,56 @@ class AzureFactory:
         else:
             print(f"Failed to create role {role_name}")
 
+    # Assign mapped MSIs to containers
+    def assign_msi_to_container(self, ddf):
+        resource_provider = "Microsoft.Storage"
+        resource_type = "storageAccounts"
+        role_permissions_map = self.get_roles_permissions_map(ddf)
+        storage_to_path_map = self.get_storage_to_container_mapping(ddf)
+        # For each role defined check the permissions
+        for role_name, permissions in role_permissions_map.items():
+            user_assigned_identity = None
+            for resource, perm, path in permissions:
+                # only move forward if resource is storage
+                if resource == 'storage':
+                    # Scope for storage container
+                    scope = "/subscriptions/{}/resourceGroups/{}/providers/{}/{}/{}/blobServices/default/containers/{}".format(
+                        self.subscription_id, self.resource_group, resource_provider, resource_type,
+                        self.storage_account_name,
+                        storage_to_path_map[path])
+                    # create only if needed
+                    if user_assigned_identity is None:
+                        user_assigned_identity = self.msi_client.user_assigned_identities.get(self.resource_group,
+                                                                                              role_name,
+                                                                                              custom_headers=None)
+                    rules = self.get_rules_for_permission(ddf, perm)
+                    policies = self.get_azure_policy(rules['Name'])
+                    role_policy = policies[0]
+
+                    try:
+                        resp = self.authorization_client.role_assignments.create(
+                            scope,
+                            uuid.uuid4(),
+                            {
+                                'role_definition_id': role_policy.id,
+                                'principal_id': user_assigned_identity.principal_id
+                            }
+                        )
+                        print(
+                            f"Successfully assigned role {rules['Name']} to MSI {user_assigned_identity.id} at scope {scope}")
+                    except CloudError as e:
+                        if 'role assignment already exists' in str(e):
+                            print(
+                                f"Role assignment for role {rules['Name']} to MSI {user_assigned_identity.id} at scope {scope} already exists")
+                        else:
+                            raise ValueError(
+                                f"Error assigning role {rules['Name']} to MSI {user_assigned_identity.id} at "
+                                f"scope {scope}, reason: {str(e)}")
+
     '''
     Function to assign policy to a given role.
     This function assumes that the MSI and policy exists.
     '''
-
     def assign_policy_to_msi(self, msi_name, policy_name):
         roles = list(self.authorization_client.role_definitions.list(self.resource_group_object.id,
                                                                      filter="roleName eq '{}'".format(policy_name)))
@@ -405,6 +449,58 @@ class AzureFactory:
                 print(f"Role: {policy_name} already attached to MSI: {msi_name}")
             else:
                 raise ValueError(f"Error attaching role: {policy_name} to MSI: {msi_name}, reason: {str(e)}")
+
+    # Helper function to get rules/policies for roles
+    # Returns a dictonary {<role_name> = [(<resource_type>,<permission>,<path>)]}
+    def get_roles_permissions_map(self, ddf):
+        role_permissions = dict()
+        for name, role in ddf['datalake_roles'].items():
+            permissions = role['permissions']
+            role_name = role['iam_role']
+            perms = set()
+            for perm in permissions:
+                elements = perm.split(':')
+                # a tuple eg (storage, read-write, /data)
+                if len(elements) == 2:
+                    perms.add((elements[0], elements[1], ''))
+                else:
+                    perms.add((elements[0], elements[1], elements[2]))
+            role_permissions[role_name] = perms
+        return role_permissions
+
+    def get_rules_for_permission(self, ddf, permission):
+        path = 'datalakes/' + ddf['datalake'] + '/Azure/'
+        filename_base = path + permission + '-policy.json'
+        if os.path.exists(filename_base):
+            with open(filename_base, "r") as policy:
+                rules = json.load(policy)
+                return rules
+        else:
+            raise ValueError(f"Could not load policy file {filename_base}")
+
+
+    # function that returns policy from azure based on provided name
+    # Returns a list, mostl likely you will need roles[0]
+    def get_azure_policy(self, policy_name):
+        # Get "Contributor" built-in role as a RoleDefinition object
+        roles = list(self.authorization_client.role_definitions.list(
+            self.resource_group_object.id,
+            filter="roleName eq '{}'".format(policy_name)
+        ))
+        return roles
+
+    # Get a map of storage location to storage path
+    # e.g.     STORAGE_LOCATION_BASE: data
+    def get_storage_to_container_mapping(self, ddf):
+        storage_to_path = dict()
+        for storage_alias, storage in ddf['storage'].items():
+            # Remove forward and trailing slashes
+            container_path = storage['path'].strip('/')
+            # in case they provided us with containers and directories
+            paths = container_path.split('/')
+            # only pick container name
+            storage_to_path[storage_alias] = paths[0]
+        return storage_to_path
 
     def __str__(self):
         return "Azure"
